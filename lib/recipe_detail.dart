5499daf458504ccdc57f37e27ed128dac9c47600
Bryan Misa <bryanmisamobiledev@gmail.com>
2025-02-03 19:37:10 +0300

    added slider

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -14,7 +14,7 @@ class RecipeDetail extends StatefulWidget {
 }
 
 class _RecipeDetailState extends State<RecipeDetail> {
-  //TODO : Add _sliderVal here
+  int _sliderVal = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,8 +38,31 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
             ),
-            // TODO: Add Expanded
-            // TODO: Add Slider() here
+            Expanded(
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  return Text(
+                      '${ingredient.quantity * _sliderVal} ${ingredient.measure} ${ingredient.name}');
+                },
+              ),
+            ),
+            Slider(
+              min: 1,
+              max: 10,
+              divisions: 9,
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              value: _sliderVal.toDouble(),
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
